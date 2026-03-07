ef7750111278e2895d653fc8b5bbf663886543b9
Mostafizur <mostafizzfuad@gmail.com>
2026-03-07 17:31:07 +0600

    chore: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: {
+		template: `%s | ${APP_NAME}`,
+		default: APP_NAME,
+	},
+	description: APP_DESCRIPTION,
+	metadataBase: new URL(SERVER_URL),
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<div className="mx-auto px-4 sm:px-0 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
+					{children}
+				</div>
+			</body>
+		</html>
+	);
+}
---
+++ lib/constants/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME =
+	process.env.NEXT_PUBLIC_APP_NAME ||
+	"Mehezabin Mehedi House - 100% Natural Mehendi Cones, Henna Powder & Herbal Hair Care in Bangladesh.";
+export const APP_DESCRIPTION =
+	process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+	"100% Natural Mehendi Cones, Henna Powder & Herbal Hair Care in Bangladesh.";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
