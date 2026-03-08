34ab9adb5bf3c231104e4f0442ff327f1ca514b1
Mostafizur <mostafizzfuad@gmail.com>
2026-03-09 00:03:14 +0600

    feat: define product types and add dummy data

---
+++ lib/types.ts
@@ -0,0 +1,18 @@
+export type ProductType = {
+	id: string | number;
+	name: string;
+	price: number;
+	images: string[];
+	categories: string[];
+	inStock: boolean;
+
+	badge?: string;
+	sku?: string;
+	rating?: number;
+	reviewsCount?: number;
+
+	shortDescription?: string[];
+	longDescription?: string;
+};
+
+export type ProductsType = ProductType[];
